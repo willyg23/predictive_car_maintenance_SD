2265a66d285b4726b0908cc8340ec4072dd6d8f3
willyg23 <willygdev@gmail.com>
2024-11-12 15:48:22 -0600

    testing tfstate lock

--- terraform/modules/storage/s3/test_bucket/main.tf
+++ terraform/modules/storage/s3/test_bucket/main.tf
@@ -6,5 +6,6 @@ resource "aws_s3_bucket" "test" {
     Test        = "state-management"  # Add a new tag for testing purposes
     Test2       = "test2" # testing again
     Test3       = "test3"
+    Test4       = "test4"
   }
 }
