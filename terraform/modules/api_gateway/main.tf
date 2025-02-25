b81ed49daf8acf8a813bc9c9f05e13c4429343c1
willyg23 <willygdev@gmail.com>
2025-02-25 12:40:30 -0600

    chore: a few more things

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -11,9 +11,10 @@ resource "aws_apigatewayv2_stage" "main" {
   auto_deploy = true
 }
 
+# TODO: change this to target lambda
 # Route for POST method
 resource "aws_apigatewayv2_route" "post" {
   api_id    = aws_apigatewayv2_api.main.id
   route_key = "POST /generate-gpt-response"
-  target    = "integrations/${aws_apigatewayv2_integration.alb.id}"
+  # target    = "integrations/${aws_apigatewayv2_integration.alb.id}" # need to change this to target a lambda, i think
 }
